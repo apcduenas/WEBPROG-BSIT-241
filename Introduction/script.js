0db1d73f9e90531816411869fccc3a0e629ac304
apcduenas <asduenas@student.apc.edu.ph>
2026-01-05 22:13:03 +0800

    UPDATE

--- Introduction/script.js
+++ Introduction/script.js
@@ -1,3 +1,3 @@
 document.getElementById("motivateBtn").addEventListener("click", function () {
-  alert("Keep pushing forward and never give up on your dreams!");
+  alert("Keep pushing forward and never give up on your dreams!:)");
 });
